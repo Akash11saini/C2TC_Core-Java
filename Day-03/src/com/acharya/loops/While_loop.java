8308a9347fb5f5a2b526c0ede98d7829acc43316
AKASH SAINI <AKASH SAINI@LAPTOP-7MBMJ6H3>
2022-06-08 20:03:21 +0530

    first commit

---
+++ Day-03/src/com/acharya/loops/While_loop.java
@@ -0,0 +1,14 @@
+package com.acharya.loops;
+
+public class While_loop {
+	public static void main(String[] args) 
+	{
+		int i=99;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
+
