5ad71ad20af170e12b11b84c3ae15ce5e7f7d0e3
AKASH SAINI <AKASH SAINI@LAPTOP-7MBMJ6H3>
2022-06-14 17:59:54 +0530

    first commit

--- ControlStatements/src/com/acharya/PredictOutput/Example4.java
+++ ControlStatements/src/com/acharya/PredictOutput/Example4.java
@@ -3,7 +3,7 @@ package com.acharya.PredictOutput;
 public class Example4 {
 	
 	public static void main(String[] args) {
-		while(true);{
+		while(true){
 			System.out.println("Technoserve");
 	}
 }
