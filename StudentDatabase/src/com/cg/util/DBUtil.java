80db9f605cb72d9c7cd3f23b12fa041c4582157c
AKASH SAINI <AKASH SAINI@LAPTOP-7MBMJ6H3>
2022-08-02 11:54:11 +0530

    first commit

---
+++ StudentDatabase/src/com/cg/util/DBUtil.java
@@ -0,0 +1,30 @@
+package com.cg.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	private static final String db_driverclass="com.mysql.cj.jdbc.Driver";
+	private static final String db_url="jdbc:mysql://localhost:3306/studentjdbc";
+	private static final String db_username="root";
+	private static final String db_password="Akash_s1112";
+	private static Connection con =null;
+	
+	static {
+		try {
+			Class.forName(db_driverclass);
+			con = DriverManager.getConnection(db_url,db_username,db_password);
+		} 
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static Connection getConnection() {
+		return con;
+	}
+
+}
